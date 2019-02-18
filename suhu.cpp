05d6dee9394ff40f268c20791632d800ca7839f5
Muhammad Rafli <44805762+RabbitCL4Y@users.noreply.github.com>
2019-02-18 19:51:44 +0800

    Create suhu.cpp

---
+++ suhu.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+int bil1, bil2;
+string lk,nm;
+int opt; char why;
+system("clear");
+veve:
+system("toilet -f pagga KALSEL[E]xploit  | lolcat");
+cout << "                Konversi SUHU   " << endl;
+cout << endl;
+cout << "\033[97mCoded By : \033[92m®Muhammad Rafli \033[97m" << endl;
+cout << "Team     : \033[92mKALSEL[E]xploit\033[97m"
+<< endl;
+cout << "Telegram : \033[92mhttps://t.me/RabbitCL4Y\033[97m" << endl;
+cout << endl;
+float celc, fahr, rea, kelv;
+cout<<" ========================================"<<endl;
+cout<<" |	Konversi Suhu : Celcius to	|"<<endl;
+cout<<" |	Fahreheit | Kelvin | Reamur	|"<<endl;
+cout<<" ========================================"<<endl;
+cout<<" Masukkan suhu dalam celcius = ";
+cin>>celc;
+
+fahr = (celc*1.8) + 32;
+rea  =  celc * 0.8;
+kelv = celc + 273.15;
+cout<<endl;
+cout<<" ========================================"<<endl;
+cout<<" |	Hasil Konversi Suhu		|"<<endl;
+cout<<" ========================================"<<endl;
+cout<<" | FAHRENHEIT  =  "<<fahr<<"			|"<<endl;
+cout<<" | REAMUR      =  "<<rea<<"			|"<<endl;
+cout<<" | KELVIN      =  "<<kelv<<"		|"<<endl;
+cout<<"========================================="<<endl;
+cout<<endl;
+cout<<" ========================================"<<endl;
+cout<<" |	Learn More ©KALSEL[E]xploit     |"<<endl;
+cout<<" ========================================"<<endl;
+
+
+}
